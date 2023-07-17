b36c7b08527e6a15f9f9073ca1d70fc2980d600f
crowdbotics-dev <engineering@crowdbotics.com>
2023-07-17 18:52:58 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+class Book(models.Model):
+    'Generated Model'
+    trial = models.CharField(max_length=255,)
+    def trial(self):
+        print("hello");
